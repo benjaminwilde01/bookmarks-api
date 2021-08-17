00bcddcc9d233f2ec04bef166e6e573bcbf421fc
Benjamin Wilde <bendavidwilde95@gmail.com>
2021-08-17 12:55:05 -0500

    working on edit functionality

--- server.js
+++ server.js
@@ -14,28 +14,22 @@ app.use(bodyParser.json())
 app.use(cors())
 
 // confugure cors middleware for other requests
-var allowlist = ['http://localhost:3003']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+// const allowlist = ['http://localhost:3003']
+// const corsOptionsDelegate = function (req, callback) {
+//   let corsOptions;
+//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
 
-app.options('*', cors()) // include before other routes
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-//   });
-  
+// app.options('*', cors()) // include before other routes
+
 // SETUP OUR MONGO
 // Error / Disconnection
 const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.eirqz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-// mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
-// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser: true,
@@ -48,7 +42,7 @@ mongoose.connection.once('open', () => {
 
 // controllers
 const bookmarksController = require('./controllers/bookmarks')
-app.use('/bookmarks', cors(corsOptionsDelegate), bookmarksController)
+app.use('/bookmarks', bookmarksController)
 
 app.get('/', (req, res) => {
     res.redirect('/bookmarks')
